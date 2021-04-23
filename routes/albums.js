b545b94243ea1b96c218baf9862b99727b008843
ianfi96 <ilfischer@uc.cl>
2021-04-23 11:56:45 -0400

    Almost almost finished 15

--- routes/albums.js
+++ routes/albums.js
@@ -52,13 +52,14 @@ router.post('/:id/tracks', async (req,res) => {
             return res.status(400).json({message: "input inválido"});
         }
         const new_track_id = Buffer.from(req.body.name + ':' + req.params.id).toString('base64').substring(0,22);
-        const track_exists = await Track.findOne({id: new_track_id}).select('-_id -__v').lean();
-        const album_exists = await Album.findOne({id: req.params.id}).select('artist_id').lean();
+        const track_exists = await Track.findOne({id: new_track_id}).select('-_id -__v');
+        const album_exists = await Album.findOne({id: req.params.id}).select('-_id -__v');
         if (!album_exists){
             return res.status(422).json({message: 'No existe el album al que se quiere agregar la cancion'});
         } else if (track_exists){
             return res.status(409).json(track_exists);
         } else {
+            const artist_exists = await Artist.findOne({id: album_exists.artist_id});
             const track = await Track.create({
                 id: new_track_id,
                 album_id: req.params.id,
